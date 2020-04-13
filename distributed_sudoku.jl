10119f5a964a1070df60ad42c831bfb2a4eeaf43
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-13 19:57:54 +0200

    almost working distributed

---
+++ distributed_sudoku.jl
@@ -0,0 +1,49 @@
+using Distributed
+@everywhere include_string(Main, $(read("sudoku.jl", String)), "sudoku.jl")
+
+function distributed_run(states, indices, temp, random_ramp_temperature)
+    max_energy = Inf
+    nsteps = random_ramp_temperature
+    while max_energy > 0
+        futures = []
+        for (i, state) in enumerate(states)
+            temp = rand()*rand(1:4)
+            push!(futures, @spawnat :2 run_mc(state, indices, temp, random_ramp_temperature))
+        end
+
+        states = [fetch(f) for f in futures]
+        max_energy = minimum([s.energy for s in states])
+        println("Max energy $max_energy")
+
+    end
+
+    return states
+    
+end
+
+
+function mc_solve(puzzle::Matrix, temp::Real, random_ramp_temperature::Int)
+    indices = get_indices(puzzle)
+    states = []
+    for i in 1:nworkers()
+        push!(states, initialize_board(puzzle, indices)) 
+    end
+    states = distributed_run(states, indices, temp, random_ramp_temperature)
+
+    return states
+    
+end
+
+puzzle = [0  0  0  0  0  0  0  9  3;
+          0  0  0  5  0  0  0  0  7;
+          0  0  8  0  7  2  0  0  5;
+          0  0  0  8  3  0  0  7  0;
+          0  0  5  0  0  0  6  0  0;
+          0  2  0  0  4  7  0  0  0;
+          8  0  0  2  9  0  1  0  0;
+          1  0  0  0  0  8  0  0  0;
+          2  4  0  0  0  0  0  0  0;]
+
+states = mc_solve(puzzle, 0.45, 100)
+
+println(states[end])
--- sudoku.jl
+++ sudoku.jl
@@ -1,6 +1,4 @@
 # Importing packages
-using Distributed
-
 
 struct SudokuState
     puzzle::Array
@@ -90,25 +88,6 @@ function random_block_swap(puzzle, indices)
     return puzzle
 end
 
-function distributed_run(states, indices, temp, random_ramp_temperature)
-    max_energy = Inf
-    nsteps = random_ramp_temperature
-    while max_energy > 0
-        futures = []
-        for state in states
-            temp = rand()*rand(1:4)
-            push!(futures, @spawnat:any run_mc(state, indices, temp, random_ramp_temperature))
-        end
-
-        states = [fetch(f) for f in futures]
-        max_energy = minimum([[s.energy for s in states]])
-
-    end
-
-    return states
-    
-end
-
 function run_mc(state, indices, temp::Real, nsteps::Int)
     count = 0
     missed_moves = 0
@@ -129,31 +108,7 @@ function run_mc(state, indices, temp::Real, nsteps::Int)
     return state
 end
 
-function mc_solve(puzzle::Matrix, temp::Real, random_ramp_temperature::Int)
-    indices = get_indices(puzzle)
-    states = []
-    for i in 1:nworkers()
-        push!(states, initialize_board(puzzle, indices)) 
-    end
-    states = distributed_run(states, indices, temp, random_ramp_temperature)
 
-    return states
-    
-end
-
-puzzle = [0  0  0  0  0  0  0  9  3;
-          0  0  0  5  0  0  0  0  7;
-          0  0  8  0  7  2  0  0  5;
-          0  0  0  8  3  0  0  7  0;
-          0  0  5  0  0  0  6  0  0;
-          0  2  0  0  4  7  0  0  0;
-          8  0  0  2  9  0  1  0  0;
-          1  0  0  0  0  8  0  0  0;
-          2  4  0  0  0  0  0  0  0;]
-
-states = mc_solve(puzzle, 0.45, 100)
-
-println(states[end])
 
 
 #[5 7 2 1 6 4 8 9 3;
