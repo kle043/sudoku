aa93a53a3fff6439041276504cdbf7f9db01de79
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-14 12:29:41 +0200

    change to golden nugget

--- sudoku.jl
+++ sudoku.jl
@@ -41,15 +41,16 @@ function mc_solve(puzzle::Matrix, nsteps::Int)
 end
 
 if !isinteractive()
-    puzzle = [0  0  0  0  0  0  0  9  3;
-              0  0  0  5  0  0  0  0  7;
-              0  0  8  0  7  2  0  0  5;
-              0  0  0  8  3  0  0  7  0;
-              0  0  5  0  0  0  6  0  0;
-              0  2  0  0  4  7  0  0  0;
-              8  0  0  2  9  0  1  0  0;
-              1  0  0  0  0  8  0  0  0;
-              2  4  0  0  0  0  0  0  0;]
+    #golden nugget
+    puzzle = [0 0 0 0 0 0 0 3 9;
+              0 0 0 0 1 0 0 0 5;
+              0 0 3 0 0 5 8 0 0;
+              0 0 8 0 0 9 0 0 6;
+              0 7 0 0 2 0 0 0 0;
+              1 0 0 4 0 0 0 0 0;
+              0 0 9 0 0 8 0 5 0;
+              0 2 0 0 0 0 6 0 0;
+              4 0 0 7 0 0 0 0 0;]
     
     println("Solving:")
     display(puzzle)
