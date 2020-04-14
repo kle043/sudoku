b60d3cbfc082b368f5c4aa0c7d1b390ac7b2121e
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-14 09:12:25 +0200

    print solution

--- distributed_sudoku.jl
+++ distributed_sudoku.jl
@@ -1,28 +1,28 @@
 using Distributed
 @everywhere include_string(Main, $(read("sudoku.jl", String)), "sudoku.jl")
 
-function distributed_run(states, indices, temp, random_ramp_temperature)
-    max_energy = Inf
-    nsteps = random_ramp_temperature
-    while max_energy > 0
+function distributed_run(states, indices, temp, nsteps)
+    min_state = states[1]
+    nsteps = nsteps
+    while min_state.energy > 0
         futures = []
         for state in states
             temp = rand()*rand(1:4)
-            push!(futures, @spawn run_mc(state, indices, temp, random_ramp_temperature))
+            push!(futures, @spawn run_mc(state, indices, temp, nsteps))
         end
 
         states = [fetch(f) for f in futures]
-        max_energy = minimum([s.energy for s in states])
-        println("Max energy $max_energy")
+        min_state = states[argmin([s.energy for s in states])]
+        println("Max energy $(min_state.energy)")
 
     end
 
-    return states
+    return states[argmin([s.energy for s in states])]
     
 end
 
 
-function mc_solve(puzzle::Matrix, temp::Real, random_ramp_temperature::Int)
+function mc_solve(puzzle::Matrix, temp::Real, nsteps::Int)
     indices = get_indices(puzzle)
     states = []
     println("Using $(nworkers()) workers")
@@ -30,7 +30,7 @@ function mc_solve(puzzle::Matrix, temp::Real, random_ramp_temperature::Int)
     for i in 1:nworkers()
         push!(states, initialize_board(puzzle, indices)) 
     end
-    states = distributed_run(states, indices, temp, random_ramp_temperature)
+    states = distributed_run(states, indices, temp, nsteps)
 
     return states
     
@@ -46,6 +46,15 @@ puzzle = [0  0  0  0  0  0  0  9  3;
           1  0  0  0  0  8  0  0  0;
           2  4  0  0  0  0  0  0  0;]
 
-@time states = mc_solve(puzzle, 0.45, 10000)
+println("Solving:")
+display(puzzle)
+println("")
 
-println(states[end])
+@time solution = mc_solve(puzzle, 0.45, 10000)
+
+println("Initial board:")
+display(puzzle)
+println("")
+println("Solution, energy=$(solution.energy)")
+display(solution.puzzle)
+println("")
