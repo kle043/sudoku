45b6678618e8318acffbea2f02feb10d7e41f504
kle043 <pale.dorg@gmail.com>
2020-09-07 15:56:16 +0200

    parse json string

--- src/Main.jl
+++ src/Main.jl
@@ -18,19 +18,29 @@ function main()
     #golden nugget
     parsed_args = parse_commandline()
 
-    puzzle = JSON.parse(parsed_args["puzzle"])
+    json_vector = JSON.parse(parsed_args["puzzle"])
+    
+    puzzle = zeros(Int8, 9, 9)
+    for i in 1:9
+        for j in 1:9
+            puzzle[i, j] = json_vector[j][i]
+        end
+    end
+    
     println("Solving:")
     display(puzzle)
-    #println("")
-    #print(JSON.json(puzzle))
-    ##@time solution = Sudoku.mc_solve(puzzle, 1500)
-    #
-    #println("Initial board:")
-    #display(puzzle)
-    #println("")
-    #println("Solution, energy=$(solution.energy)")
-    #display(solution.puzzle)
-    #println("")
+    println("")
+
+    @time solution = Sudoku.mc_solve(puzzle, 1500)
+    
+    println("Initial board:")
+    display(puzzle)
+    println("")
+    println("Solution, energy=$(solution.energy)")
+    display(solution.puzzle)
+    println("")
 end
 
 main()
+
+# julia src/Main.jl [[0,0,0,0,0,1,0,0,4],[0,0,0,0,7,0,0,2,0],[0,0,3,8,0,0,9,0,0],[0,0,0,0,0,4,0,0,7],[0,1,0,0,2,0,0,0,0],[0,0,5,9,0,0,8,0,0],[0,0,8,0,0,0,0,6,0],[3,0,0,0,0,0,5,0,0],[9,5,0,6,0,0,0,0,0]]
